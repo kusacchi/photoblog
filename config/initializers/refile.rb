9849d4077a43ce6365a71a6b2579045b5383e4db
kusacchi <66767439+kusacchi@users.noreply.github.com>
2020-10-26 16:28:25 +0900

    repair refile.rb

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,10 @@
-require 'refile/s3'
+require "refile/s3"
 
-if Rails.env.production? # 本番環境の場合はS3へアップロード
-  aws = {
-    access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-    region: 'ap-northeast-1', # リージョン
-    bucket: 'photo-blog', # バケット名
-  }
-  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
-  Refile.store = Refile::S3.new(prefix: 'store', **aws)
-end
+aws = {
+  access_key_id: "AWS_ACCESS_KEY_ID",
+  secret_access_key: "AWS_SECRET_ACCESS_KEY",
+  region: "ap-northeast-1",
+  bucket: "photo-blog",
+}
+Refile.cache = Refile::S3.new(prefix: "cache", **aws)
+Refile.store = Refile::S3.new(prefix: "store", **aws)
